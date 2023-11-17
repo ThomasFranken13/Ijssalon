aac59e80cd2fec80c3f349102e938c1d409c30b7
Thomas <thomas.franken.nl@gmail.com>
2023-11-17 13:41:07 +0100

    push van loops

---
+++ Les5/Les5.py
@@ -0,0 +1,4 @@
+A = input("welk getal wilt u gebruiken?")
+B = int(A)
+C = B * 2
+print(C)
---
+++ Les5/loops.py
@@ -0,0 +1,20 @@
+i = 0
+while i < 6:
+    print(i)
+    i = i + 1
+print("eind")
+
+# While loop
+
+for i in range (1,3):
+    print("enkel:", i)
+    print("dubbel:", i * 2)
+print("en we zijn weer uit de loop")
+
+# For loop
+
+for i in range (0,10,-1):
+    print(i)
+print("Lift Off!")
+
+#for loop
